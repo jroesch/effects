fca06ecdf0f123bf4277029e3430765d6af39b66
Jared Roesch <roeschinc@gmail.com>
2016-10-11 22:53:17 -0700

    Trying to understand how to implement pure

--- dijkstra2.lean
+++ dijkstra2.lean
@@ -12,13 +12,16 @@ namespace pure
   def push {A T U : Type} (f : (T → context A U)) : context A (T -> U) :=
     fun p e1, f e1 p
 
-  def {u} wp (A : Type u) := context A Prop
+  @[reducible] def {u} wp (A : Type u) := context A Prop
   
   def return_wp {A : Type} (x : A) : wp A :=
     fun p, p x
 
   def bind_wp (A B : Type) (wp1 : wp A) (wp2 : A → wp B) : wp B :=
     fun p, wp1 (fun (x : A), wp2 x p)
+
+  def strengthen (A : Type) (wp1 : wp A) (wp2 : wp A) :=
+    forall (p1 p2 : A → Prop), (forall x, p1 x -> p2 x) -> (wp1 p1 -> wp2 p2)
 end pure
 
 constant PURE (T : Type) (wp : pure.wp T) : Type
@@ -31,7 +34,21 @@ begin
   tactic.unfold [`pure.return_wp],
 end
 
-def test (n : nat) : Pure nat (n > 0) (fun m, m = n) :=
+definition sqrt' (x : nat) : PURE nat (fun (post : nat → Prop), forall y, y >= 0 → post y) := sorry
+
+def weaken :
+  forall (p q : pure.wp nat), (forall post, p post -> q post) -> PURE nat p -> PURE nat q :=
+begin
+  intros,
+end
+
+definition sqrt (x : nat) : Pure nat true (fun y, y >= 0) :=
 begin
   tactic.unfold [`Pure],
+  apply sqrt',
+end
+
+def test (n : nat) : Pure nat (n > 0) (fun m, m = n) :=
+begin
+  tactic.unfold [`Pure]
 end
